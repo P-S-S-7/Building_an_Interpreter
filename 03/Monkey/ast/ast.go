08155c2b0d8ffc6943aa54002ee6ac35c7f0e2af
P-S-S-7 <psinghskekhawat982@gmail.com>
2024-06-10 04:59:55 +0530

    parsing expressions(Pratt parsing).Till now parsed identifiers, integer literals and prefix operators

--- 03/Monkey/ast/ast.go
+++ 03/Monkey/ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "Monkey/token"
+import (
+	"Monkey/token"
+	"bytes"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -29,7 +33,15 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-// let
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+// let --- node type 1
 type LetStatement struct {
 	Token token.Token //the token.LET token
 	Name  *Identifier
@@ -38,7 +50,21 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
 
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+// struct to hold the value of expression or parse identifier expression
 type Identifier struct {
 	Token token.Token //the token.IDENT token
 	Value string
@@ -46,8 +72,9 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
-// return
+// return --- node type 2
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
@@ -55,3 +82,60 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+// --- node type 3
+type ExpressionStatement struct {
+	Token      token.Token //the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+//expressions
+
+// struct to parse integer literal expression
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64 //actual value of the integer literal
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+// struct to parse prefix operator expression
+type PrefixExpression struct {
+	Token    token.Token //The prefix token, e.g. !
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Building_an_Interpreter
+
+# Identifiers can act as expressions
+# Identifiers can also hold the value of an expression 
