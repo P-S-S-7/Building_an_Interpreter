e2433e1375163e4d70546c7721fdee2fbfc94209
P-S-S-7 <psinghskekhawat982@gmail.com>
2024-06-12 20:09:09 +0530

    parsed function literals

--- 03/Monkey/ast/ast.go
+++ 03/Monkey/ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"Monkey/token"
 	"bytes"
+	"strings"
 )
 
 type Node interface {
@@ -196,12 +197,36 @@ func (ie *IfExpression) String() string {
 	return out.String()
 }
 
+// struct for function literal expression
+type FunctionLiteral struct {
+	Token      token.Token // the 'fn' token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
+
 type BlockStatement struct {
 	Token      token.Token // the { token
 	Statements []Statement
 }
 
-func (bs *BlockStatement) expressionNode()      {}
+func (bs *BlockStatement) statementNode()       {}
 func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
 func (bs *BlockStatement) String() string {
 	var out bytes.Buffer
