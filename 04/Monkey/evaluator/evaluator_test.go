cc6d90aa83f8b2460cf123be01cafa188ae95a74
P-S-S-7 <psinghskekhawat982@gmail.com>
2024-06-17 03:43:12 +0530

    evaluated infix expressions for both integer and boolean operands...Now basically we have a fully functional calculator

--- 04/Monkey/evaluator/evaluator_test.go
+++ 04/Monkey/evaluator/evaluator_test.go
@@ -15,7 +15,7 @@ func testEval(input string) object.Object {
 	return Eval(program)
 }
 
-// test evaluation of object.Integer
+// test evaluation of object.Integer returning expressions
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -25,6 +25,17 @@ func TestEvalIntegerExpression(t *testing.T) {
 		{"10", 10},
 		{"-5", -5},
 		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -46,7 +57,7 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	return true
 }
 
-// test evaluation of object.Boolean
+// test evaluation of object.Boolean returning expressions
 func TestEvalBooleanExpression(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -54,6 +65,23 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -96,4 +124,9 @@ func TestBangOperator(t *testing.T) {
 }
 
 // prefix expression operator II - MINUS Operator
-//see extended func TestEvalIntegerExpression(t *testing.T)
+// see extended TestEvalIntegerExpression(t *testing.T) as it returns object.Integer
+
+// test evaluation of infix expressions
+// we have extended the TestEvalIntegerExpression(t *testing.T) for expressions with integers as operands and whose return value is a object.Integer.
+
+// we have extended the TestEvalBooleanExpression(t *testing.T) for expressions with integers or booleans as operands and whose return value is a object.Boolean.
