6170ea5ac60129b58cf371246914709c0cab4d45
P-S-S-7 <psinghskekhawat982@gmail.com>
2024-06-20 19:47:43 +0530

    added bindings and created environment (Now our interpreter has memory) - evaluated let statements and identifiers

--- 04/Monkey/evaluator/evaluator_test.go
+++ 04/Monkey/evaluator/evaluator_test.go
@@ -11,8 +11,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 // test evaluation of object.Integer returning expressions
@@ -185,7 +186,6 @@ func TestReturnStatements(t *testing.T) {
 		} 
 		`, 10},
 	}
-	// nil is a type (here of interface)
 
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -209,7 +209,7 @@ func TestErrorHandling(t *testing.T) {
 		},
 		{
 			"-true",
-			"unknown operator: -BOOLEAN",
+			"unknown operand: -BOOLEAN",
 		},
 		{
 			"true + false;",
@@ -224,15 +224,15 @@ func TestErrorHandling(t *testing.T) {
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
 		{
-			`
-		if (10 > 1) {
-		if (10 > 1) {
-		return true + false;
-		}
-		return 1;
-		}
-		`,
-			"unknown operator: BOOLEAN + BOOLEAN",
+			`if (10 > 1) {
+				if (10 > 1) {
+					return true + false;
+				}
+				return 1;
+		 	}`, "unknown operator: BOOLEAN + BOOLEAN",
+		},
+		{
+			"foobar", "identifier not found: foobar",
 		},
 	}
 
@@ -248,5 +248,23 @@ func TestErrorHandling(t *testing.T) {
 		if errObj.Message != tt.expectedMessage {
 			t.Errorf("wrong error message, expected = %q, got = %q", tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+// test evaluation of let statements
+func TestRLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
 	}
 }
